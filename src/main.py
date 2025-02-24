037b828dc11a8adcce8d0a4fe536c9c7e7db8c86
Ihs4nn <ihsan.fikry@vodafone.com>
2025-02-24 17:43:14 +0000

    UT07 tests passing

--- src/main.py
+++ src/main.py
@@ -1,6 +1,37 @@
 import pygame
+import sys
 from src.game import Game
+from src.game import Board
 
+class GameScreen:
+    def __init__(self, width, height):
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((width, height))
+        # Title of the game
+        self.title = "Flip 'n' Find"
+        pygame.display.set_caption(self.title)
+        # Start and Exit buttons
+        self.start_button = pygame.Rect(300, 200, 200, 50)
+        self.exit_button = pygame.Rect(300, 300, 200, 50)
+        self.board = Board(self.width, self.height)
+        self.game = Game(self.width, self.height, self.board)
+        self.game_state = "MENU"
 
-class StartMenu:
+    def handle_click(self):
+        # Gets the events
+        for event in pygame.event.get():
+            # Only check for button collisions on mouse click events
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Checks if start button was clicked
+                if self.start_button.collidepoint(event.pos):
+                    # Change game state and start a new game
+                    self.game_state = "PLAY"
+                    self.game.start_new_game()
+                # Checks if the exit button was clicked
+                elif self.exit_button.collidepoint(event.pos):
+                    pygame.quit()
+                    sys.exit()
+
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,59 @@
 import pytest
 import pygame
-from src.main import StartMenu
+import sys
+from pygame import event
+from pygame.rect import Rect
+from src.main import GameScreen
 
 @pytest.fixture
-def menu():
+def start():
     pygame.init()
-    menu = StartMenu(width=800, height=600)
-    return menu
+    start = GameScreen(800, 600)
+    return start
 
 class TestUT07:
     # Step 1
-    def test_menu_initalisation(self, menu):
-        assert menu.title == "Flip 'n' Find"
-        assert menu.start_button.x == 300
-        assert menu.start_button.y == 200
-        assert menu.exit_button.x == 300
-        assert menu.exit_button.y == 300
+    def test_menu_initalisation(self, start):
+        # Check if it has the correct title
+        assert start.title == "Flip 'n' Find"
+        # Check if the buttons have the correct dimensions
+        assert start.start_button.x == 300
+        assert start.start_button.y == 200
+        assert start.exit_button.x == 300
+        assert start.exit_button.y == 300
+    # Step 2
+    def test_user_click_start(self, start):
+        # Check it is first in the main menu
+        assert start.game_state == "MENU"
+        # Handle user mouse click on 'Start'
+        click_start = pygame.event.Event(pygame.MOUSEBUTTONDOWN, {'pos': (350, 225)})
+        pygame.event.post(click_start)
+        # Call function used to handle the clicks
+        start.handle_click()
+        # Checks if based on the click, the event state changes accordingly
+        assert start.game_state == "PLAY"
+        # Check if the game initalises
+        assert len(start.game.board.cards) > 0
+    # Step 3
+    def test_user_click_exit(self, start, mocker):
+        # Mocking functions used to quit and exit the application
+        mocker.patch('pygame.quit')
+        mocker.patch('sys.exit')
+        assert start.game_state == "MENU"
+        click_exit = pygame.event.Event(pygame.MOUSEBUTTONDOWN, {'pos': (350, 325)})
+        pygame.event.post(click_exit)
+        start.handle_click()
+        # Check if the application does close
+        pygame.quit.assert_called_once()
+        sys.exit.assert_called_once()
+
+
+
+
+
+        
+
+
+
+
+
