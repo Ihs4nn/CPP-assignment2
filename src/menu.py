4eed51e567da0b64df79b6b1775c51b234174698
Ihs4nn <ihsan.fikry@vodafone.com>
2025-02-24 13:34:22 +0000

    Starting coding UT07

---
+++ src/menu.py
@@ -0,0 +1,9 @@
+import pygame
+import button
+import sys
+from src.game import Game
+
+# Initalise main menu with title page
+# Game states? MENU, PLAYING, EXIT?
+# Clock feature?
+# 
---
+++ tests/test_menu.py
@@ -0,0 +1,6 @@
+import pytest
+import pygame
+import sys
+from src.game import Game
+
+
